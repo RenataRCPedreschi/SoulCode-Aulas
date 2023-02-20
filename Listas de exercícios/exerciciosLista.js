cfa15579c708abf3769466a07a675ccd5a5d3b6a
RenataRCPedreschi <renatarabelocastro@gmail.com>
2023-02-20 01:58:36 -0300

    Arquivo Criado

---
+++ Listas de exercícios/exerciciosLista.js
@@ -0,0 +1,25 @@
+//Variáveis e operadores
+
+/* Crie uma variável que armazene uma temperatura em Fahrenheit, e re-
+alize a conversão para Celsius. A equação para realizar a conversão é:
+
+C =
+F − 32
+1.8 */
+
+/* Crie duas variáveis nome e sobrenome. Crie uma nova variável chamada
+nomeCompleto e realize a concatenação de nome e sobrenome com
+template strings. */
+
+/* Crie cinco variáveis que representam cinco notas de um estudante. Cal-
+cule a média ponderada, sabendo que o peso das notas são respectiva-
+mente: 3, 2, 1, 4 e 5.
+ */
+
+/* Crie duas variáveis que representam as dimensões (em km) de um ter-
+reno: comprimento e largura. Calcule a área deste terreno, em seguida converta para hectares. Nota: 1 km2 equivale a 100 hectares. */
+
+/* Crie uma variável que armazena uma temperatura em Kelvin. Converta
+para Celsius. A equação é:
+
+C = K − 273 */
