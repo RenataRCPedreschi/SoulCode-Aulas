d2334544a469d87b2300dad0f7c2a7fac2424fd9
RenataRCPedreschi <renatarabelocastro@gmail.com>
2023-02-22 17:22:22 -0300

    Exercícios de variáveis ok

---
+++ Listas de exercícios/estruturasCondicionais.js
@@ -0,0 +1,24 @@
+//Estruturas condicionais
+/* 1. Crie três variáveis: a, b e c. Estas variáveis armazenam números. Exiba qual destes números é o maior. */
+
+
+
+/* 2. Crie três variáveis que representam os três lados de um triângulo. Aplica a seguinte regra para definir se o triângulo é possível: Se a soma de dois lados é maior que o terceiro lado. */
+
+
+
+
+/* 3. Crie uma variável idade. Caso seja uma idade inválida (negativa ou maior que 130) mostre uma mensagem de aviso. */
+
+
+
+/* 4. Crie duas variáveis: salário e quantidadeDependentes. Siga a seguinte regra para aplicar ajustes salariais: Caso não possua dependentes não realizar ajustes no salário, se houver entre 1 e 5 aumentar em 30% o salário. Caso seja acima de 5 aumentar em 40% o salário.*/
+
+
+
+
+/* 5. Crie uma variável para armazenar um dia da semana (1 - Domingo, 2 - Segunda, ... 7 -Sábado). Com base no valor indique se é final de semana ou dia de útil. */
+
+
+
+/* 6. Crie uma variável ano e verifique se este ano é bissexto. Um ano é bissexto quando: Caso 1) É um número divisível por 4, mas não é divisível por 100. Caso 2) É um número divisível por 4, por 100 e por 400. */
---
+++ Listas de exercícios/variaveisOperadores.js
@@ -0,0 +1,58 @@
+//Variáveis e operadores
+
+/* 1 - Crie uma variável que armazene uma temperatura em Fahrenheit, e realize a conversão para Celsius. A equação para realizar a conversão é:
+C = F − 32 / 1.8 */
+
+let fahrenheit = 65;
+
+let celsius =  (fahrenheit - 32)/1.8;
+
+console.log(`A temperatura ficou ${celsius.toFixed(1)} ºC`);
+
+
+
+/* 2 - Crie duas variáveis nome e sobrenome. Crie uma nova variável chamada nomeCompleto e realize a concatenação de nome e sobrenome com template strings. */
+
+let nome = "Renata";
+let sobrenome = "Rabelo";
+
+let nomeCompleto = `${nome} ${sobrenome}`;
+
+console.log(nomeCompleto);
+
+
+
+
+/* 3 - Crie cinco variáveis que representam cinco notas de um estudante. Calcule a média ponderada, sabendo que o peso das notas são respectivamente: 3, 2, 1, 4 e 5.
+ */
+
+let n1 = 3;
+let n2 = 2;
+let n3 = 1;
+let n4 = 4;
+let n5 = 5;
+let mediaNotas = (n1, n2, n3, n4, n5)/5;
+
+console.log(`A média das notas é: ${mediaNotas}`);
+
+
+
+/* 4 - Crie duas variáveis que representam as dimensões (em km) de um terreno: comprimento e largura. Calcule a área deste terreno, em seguida converta para hectares. Nota: 1 km2 equivale a 100 hectares. */
+
+let comprimento = 35;
+let largura = 50;
+
+let areaTerreno = comprimento * largura;
+
+let converterKmparaHectare = areaTerreno*100;
+
+
+console.log(`O terreno tem ${areaTerreno} Km, que seria ${converterKmparaHectare} em Hectare.`);
+
+
+/* 5 - Crie uma variável que armazena uma temperatura em Kelvin. Converta para Celsius. A equação é: C = K − 273 */
+
+let k = 350;
+let c = k - 273;
+
+console.log(`A temperatura está em ${c}ºC.`);
